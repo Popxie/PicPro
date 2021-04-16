48ea28a7c198d2425622d5834d9a40a9d599d441
KaKa_Xie <mrxiehuaqiang@163.com>
2021-04-16 21:10:58 +0800

    项目初始化

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <div class="main-cont">
+      <div class="test">3333</div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+}
+</script>
+
+<style>
+* {
+  margin: 0;
+}
+.main-cont {
+  margin: 0 auto;
+  width: 1080px;
+  height: 1920px;
+  border: 1px solid;
+}
+.test {
+  width: 100%;
+  height: 40px;
+  border: 1px solid red;
+}
+</style>
---
+++ vue.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  css: {
+    loaderOptions: {
+      postcss: {
+        plugins: [
+          require('postcss-plugin-px2rem')({
+            /**
+             * 换算基数， 默认100。
+             * 这样的话把根标签的字体规定为1rem为`${rootValue}px`,
+             * 这样就可以从设计稿上量出多少个px直接在代码中写多上px了。
+             */
+            rootValue: 108, 
+            /**
+             * 默认false，可以（reg）利用正则表达式排除某些文件夹的方法，
+             * 例如/(node_module)\/如果想把前端UI框架内的px也转换成rem，
+             * 请把此属性设为默认值
+             */
+            exclude: /(node_module)/,
+            //（布尔值）允许在媒体查询中转换px。
+            mediaQuery: false,
+          }),
+        ]
+      }
+    }
+  },
+}
